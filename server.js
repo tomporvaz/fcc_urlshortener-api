b7c7857331259af5bbae4cdb22bf8563e371156c
tomporvaz <tomporvaz@gmail.com>
2019-07-03 19:30:16 -0400

    Deleted a lot of old code, and commented out code.  Also commented final solution for createEntry function in the .../new route.

--- server.js
+++ server.js
@@ -48,17 +48,21 @@ app.post("/api/shorturl/new", function (req, res) {
   let parsedURL = urlParser(req.body.url);
   console.log("ParsedURL: " + JSON.stringify(parsedURL));
   
-  let retry = false; //used to test if url should be resaved.
-  
+  //dns.lookup checks if url is valid.
   dns.lookup(parsedURL.hostname, 
     function(err){
+      //If url is invalid, then error is logged and response to user notes the error
       if(err){ 
         res.json({"url": req.body.url, "error": "invalid url"})
         console.error("url not found! error: " + err)
       } else {
-        //query mongo to find available shorturl
-        //add parsedURL object to mongo with a shorturl
-        //The do while loop will retry a new random number until save is sucessful
+        /*
+        createEntry function creates a URLentry document with the current URL,
+        document.save() saves new URLentry to the database, and with an 11000 error
+        indicating a database collision, createEntry() function is recursively called 
+        with an attemptCounter incremented up to 10 attempts, at which point an error will
+        be thrown, and the range of unique numbers should be increased.
+        */
         function createEntry(requestBodyURL, attemptCounter) {
           if (attemptCounter > 10){
             res.json({Error: "Too many attempts to save shortURL"});
@@ -86,120 +90,61 @@ app.post("/api/shorturl/new", function (req, res) {
               
             }
             )
-            
-            /*
-            return new Promise(function (resolve) {
-              let entryPromise = createSaveShortURL(req.body.url);
-              entryPromise
-              .then(entry => function(entry){
-                console.log("Returned Entry: " + JSON.stringify(entry));
-                return res.json({"URL": entry.url, "ShortURL": entry.shortURL});
-              } .catch(
-                (error) => {
-                  console.error("CurrentURL could not be saved. " + error);
-                  if(error.code === 11000){
-                    console.log("Error code 11000");
-                    attemptCounter += attemptCounter;
-                    createEntry(url, attemptCounter);
-                  }
-                }
-                )
-                
-                )
-              })*/
-            }
-            createEntry(req.body.url, 0);
           }
-        })
-      });
-      
-      
-      /*url parser function to split url into:
-      - protocol (e.g. https://),
-      - hostname (e.g. www.freecodecamp.org)
-      - path (e.g. /) 
-      */
-      function urlParser(url) {
-        let protocol = "";
-        let hostname = "";
-        let path = "";
-        
-        //split protocol off of url
-        if(url.startsWith("https://")){
-        protocol = "https://"
-      } else if (url.startsWith("http://")){
-      protocol = "http://"
-    } else {
-      //return error here and escape function
-      console.error("url does not contain a protocol")}
-      
-      //split hostname off of url
-      const beginSlice = protocol.length;
-      console.log("BeginSlice: " + beginSlice);
-      const endSlice = url.indexOf("/", (protocol.length + 1));
-      console.log("EndSlice: " + endSlice);
-      
-      if (endSlice > -1){
-        hostname = url.slice(beginSlice, endSlice);
-      } else {hostname = url.slice(beginSlice)};
-      
-      //split path off of url
-      if (endSlice > -1){
-        path = url.slice(endSlice);
-      }
-      
-      
-      
-      //return object with url, protocol, hostname, and path
-      return {
-        "url": url,
-        "protocol": protocol,
-        "hostname": hostname,
-        "path": path
-      }
-    }
-    
-    function getRandomInt(max) {
-      return Math.floor(Math.random() * Math.floor(max));
-    }
-    
-    
-    //DELETE ALL CODE HERE too....
-    function createSaveShortURL (requestBodyURL) {
-      const currentURL = new URLentry({url: requestBodyURL, shortURL: getRandomInt(10000)});
-      return currentURL.save()
-      /*.then(
-        savedEntry => savedEntry, //return saved object
-        //begining of failure callback the recursively calls createSaveShortURL on shortURL db collision
-        function(error){  
-          console.error("CurrentURL could not be saved. " + error);
-          if(error.code === 11000){
-            console.log("Error code 11000");
-            createSaveShortURL(requestBodyURL);
-          }
-        }*/
-        // );
-        /*
-        function(err, entry){
-          if(err){
-            console.error("CurrentURL could not be saved. " + err);
-            
-            //check if error occured due to a shortURL collision and set retry to true if so
-            //could be implemented using info from error (less resilent due to mongoose updates)
-            //or it could be implemented with a query (ineffecient due to additional db request)
-            if(err.code === 11000){
-              console.log("Error code 11000");
-              createSaveShortURL(requestBodyURL);
-            }
-          };
-          console.log("Saved entry: " + JSON.stringify(entry));
-          
+          createEntry(req.body.url, 0);
         }
-        );
-        */
-      };
-      //....HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      })
+    });
+    
+    
+    /*url parser function to split url into:
+    - protocol (e.g. https://),
+    - hostname (e.g. www.freecodecamp.org)
+    - path (e.g. /) 
+    */
+    function urlParser(url) {
+      let protocol = "";
+      let hostname = "";
+      let path = "";
       
-      app.listen(port, function () {
-        console.log('Node.js listening ...');
-      });
+      //split protocol off of url
+      if(url.startsWith("https://")){
+      protocol = "https://"
+    } else if (url.startsWith("http://")){
+    protocol = "http://"
+  } else {
+    //return error here and escape function
+    console.error("url does not contain a protocol")}
+    
+    //split hostname off of url
+    const beginSlice = protocol.length;
+    console.log("BeginSlice: " + beginSlice);
+    const endSlice = url.indexOf("/", (protocol.length + 1));
+    console.log("EndSlice: " + endSlice);
+    
+    if (endSlice > -1){
+      hostname = url.slice(beginSlice, endSlice);
+    } else {hostname = url.slice(beginSlice)};
+    
+    //split path off of url
+    if (endSlice > -1){
+      path = url.slice(endSlice);
+    }
+    
+    //return object with url, protocol, hostname, and path
+    return {
+      "url": url,
+      "protocol": protocol,
+      "hostname": hostname,
+      "path": path
+    }
+  }
+  
+  
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+  
+  app.listen(port, function () {
+    console.log('Node.js listening ...');
+  });
